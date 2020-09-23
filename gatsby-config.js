346984b75a540464f2252b4bdb7c53c115b31e81
Burak Yigit Kaya <ben@byk.im>
2020-09-23 18:07:37 +0300

    feat(analytics): Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,21 +13,37 @@ const queries = require("./src/utils/algolia");
 const getPlugins = () => {
   const plugins = [
     {
-      resolve: "@sentry/gatsby",
+      resolve: '@sentry/gatsby',
       options: {
         dsn:
           process.env.SENTRY_DSN ||
-          "https://f107f3f0deb544289e4e056922e5e5a4@o1.ingest.sentry.io/5266138",
-        tracesSampleRate: activeEnv === "development" ? 0 : 1,
+          'https://f107f3f0deb544289e4e056922e5e5a4@o1.ingest.sentry.io/5266138',
+        tracesSampleRate: activeEnv === 'development' ? 0 : 1,
       },
     },
-    "gatsby-plugin-sass",
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-zeit-now",
+    'gatsby-plugin-sass',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-zeit-now',
     {
-      resolve: "gatsby-plugin-mdx",
+      resolve: 'gatsby-plugin-google-gtag',
       options: {
-        remarkPlugins: [require("remark-deflist")],
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'UA-30327640-1', // Sentry
+          'UA-30327640-3', // www
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        remarkPlugins: [require('remark-deflist')],
         gatsbyRemarkPlugins: [
           {
             resolve: `gatsby-remark-copy-linked-files`,
@@ -35,7 +51,7 @@ const getPlugins = () => {
           {
             resolve: `gatsby-remark-autolink-headers`,
             options: {
-              className: "anchor",
+              className: 'anchor',
               icon: `<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M10.879 6.05L15 1.93A5.001 5.001 0 0 1 22.071 9l-4.121 4.121a1 1 0 0 1-1.414-1.414l4.12-4.121a3 3 0 1 0-4.242-4.243l-4.121 4.121a1 1 0 1 1-1.414-1.414zm2.242 11.9L9 22.07A5 5 0 1 1 1.929 15l4.121-4.121a1 1 0 0 1 1.414 1.414l-4.12 4.121a3 3 0 1 0 4.242 4.243l4.121-4.121a1 1 0 1 1 1.414 1.414zm-8.364-.122l13.071-13.07a1 1 0 0 1 1.415 1.414L6.172 19.242a1 1 0 1 1-1.415-1.414z" fill="currentColor"></path></svg>`,
               enableCustomId: true,
             },
@@ -48,16 +64,16 @@ const getPlugins = () => {
             },
           },
           {
-            resolve: "gatsby-remark-mermaid",
+            resolve: 'gatsby-remark-mermaid',
           },
           {
-            resolve: require.resolve("./plugins/gatsby-plugin-code-tabs"),
+            resolve: require.resolve('./plugins/gatsby-plugin-code-tabs'),
           },
           {
-            resolve: require.resolve("./plugins/gatsby-plugin-include"),
+            resolve: require.resolve('./plugins/gatsby-plugin-include'),
           },
           {
-            resolve: "gatsby-remark-prismjs",
+            resolve: 'gatsby-remark-prismjs',
             options: {
               noInlineHighlight: true,
             },
@@ -68,7 +84,7 @@ const getPlugins = () => {
         ],
       },
     },
-    "gatsby-plugin-react-helmet",
+    'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -91,7 +107,7 @@ const getPlugins = () => {
       },
     },
     {
-      resolve: require.resolve("./plugins/gatsby-plugin-jsonschema"),
+      resolve: require.resolve('./plugins/gatsby-plugin-jsonschema'),
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
