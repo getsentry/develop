327d7791c312a161c112a09fc62f055e0d759ccb
Burak Yigit Kaya <ben@byk.im>
2020-09-24 21:19:57 +0300

    better visuals

--- src/components/createGitHubAppForm.js
+++ src/components/createGitHubAppForm.js
@@ -1,14 +1,25 @@
-import React, {useState, useRef} from 'react';
+import React, { useState, useRef } from "react";
 
-const CreateGitHubAppForm = ({url, defaultOrg, defaultUrlPrefix, ...props}) => {
+const MAX_COMPONENTS_ON_PAGE = 100;
+
+const CreateGitHubAppForm = ({
+  url,
+  defaultOrg,
+  defaultUrlPrefix,
+  ...props
+}) => {
   const [orgSlug, setOrgSlug] = useState(defaultOrg);
   const [urlPrefix, setUrlPrefix] = useState(defaultUrlPrefix);
   const renderedUrl = url
     .replace(/:org(?=\W)/g, orgSlug)
     .replace(/:url-prefix(?=\W)/g, encodeURIComponent(urlPrefix));
 
+  const labelStyle = {
+    margin: "0.5rem",
+  };
+
   // This is to avoid in case multiple instances of this component are used on the page
-  const randomCounter = Math.random();
+  const randomCounter = Math.round(Math.random() * MAX_COMPONENTS_ON_PAGE);
 
   const orgSlugRef = useRef();
   const urlPrefixRef = useRef();
@@ -16,9 +27,9 @@ const CreateGitHubAppForm = ({url, defaultOrg, defaultUrlPrefix, ...props}) => {
   return (
     <table>
       <tr>
-        <td>
-          <label htmlFor={`gh-org-slug-${randomCounter}`}>
-            GitHub Org Slug:
+        <td align="right">
+          <label htmlFor={`gh-org-slug-${randomCounter}`} style={labelStyle}>
+            GitHub Org Slug
           </label>
         </td>
         <td>
@@ -27,14 +38,18 @@ const CreateGitHubAppForm = ({url, defaultOrg, defaultUrlPrefix, ...props}) => {
             value={orgSlug}
             ref={orgSlugRef}
             size={30}
+            className="form-control"
             onChange={() => setOrgSlug(orgSlugRef.current.value)}
           />
         </td>
       </tr>
       <tr>
-        <td>
-          <label htmlFor={`sentry-url-prefix-${randomCounter}`}>
-            Sentry <code>url-prefix</code>:
+        <td align="right" className="content-flush-bottom">
+          <label
+            htmlFor={`sentry-url-prefix-${randomCounter}`}
+            style={labelStyle}
+          >
+            Sentry <code>url-prefix</code>
           </label>
         </td>
         <td>
@@ -43,16 +58,18 @@ const CreateGitHubAppForm = ({url, defaultOrg, defaultUrlPrefix, ...props}) => {
             value={urlPrefix}
             ref={urlPrefixRef}
             size={30}
+            className="form-control"
             onChange={() => setUrlPrefix(urlPrefixRef.current.value)}
           />
         </td>
       </tr>
       <tr>
         <td colSpan="2" align="right">
-          {' '}
+          {" "}
           <button
             type="button"
-            onClick={() => window.open(renderedUrl, '_blank', 'noopener')}
+            className="btn btn-primary d-block d-inline-block-md"
+            onClick={() => window.open(renderedUrl, "_blank", "noopener")}
           >
             Create GitHub App
           </button>
