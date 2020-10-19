edb5c41fa3f836c51cd534bc6cc6e384824456bd
Burak Yigit Kaya <ben@byk.im>
2020-10-19 20:52:12 +0300

    perf(prefetching): Disable prefetching to preserve bw

---
+++ gatsby-browser.js
@@ -0,0 +1,6 @@
+// Disable prefetching altogether so our bw is not destroyed.
+// If this turns out to hurt performance significantly, we can
+// try out https://www.npmjs.com/package/gatsby-plugin-guess-js
+// with data from the prior 1-2 weeks.
+export const disableCorePrefetching = () => true;
+
