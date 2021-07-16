9e23d40bc60046a9abd9966bff7f05bc2b289afb
Chad Whitacre <chadwhitacre@sentry.io>
2021-07-16 11:32:37 -0400

    A couple more small changes
    - Fiddle with nav
    - Fiddle with formatting

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -103,11 +103,11 @@ export default () => {
         </div>
         <ul className="list-unstyled" data-sidebar-tree>
           <SidebarLink to="/self-hosted/">Overview</SidebarLink>
+          <SidebarLink to="/self-hosted/custom-ca-roots/">Custom CA Roots</SidebarLink>
           <SidebarLink to="/self-hosted/email/">Email</SidebarLink>
           <SidebarLink to="/self-hosted/geolocation/">Geolocation</SidebarLink>
           <SidebarLink to="/self-hosted/sso/">Single Sign-On (SSO)</SidebarLink>
           <SidebarLink to="/self-hosted/backup/">Backup & Restore</SidebarLink>
-          <SidebarLink to="/self-hosted/custom-ca-roots/">Custom CA Roots</SidebarLink>
           <SidebarLink to="/self-hosted/releases/">
             Versioning & Releases
           </SidebarLink>
