f927b9dbd11e626f8051b42f1891c8e95b1a9e69
Abhijeet Prasad <aprasad@sentry.io>
2021-12-02 11:33:21 -0500

    feat(perf): Document span operations spec (#452)
    This PR adds documentation around the span operations spec that we use. Span operations are a short code identifying the type of operation the span is measuring. Span operations are low cardinality attributes - they should be as general as possible while still being human readable and useful. They should avoid including high cardinality data like IDs and URLs.
    
    For the first part of the spec, we add a table of commonly used span operations as well as some background. In a follow up patch, we will formally document the structure of span operations as part of the spec.
    
    Co-authored-by: Rodolfo Carvalho <rodolfo.carvalho@sentry.io>

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -207,7 +207,8 @@ export default () => {
           <SidebarLink to="/sdk/envelopes/">Envelopes</SidebarLink>
           <SidebarLink to="/sdk/rate-limiting/">Rate Limiting</SidebarLink>
           <SidebarLink to="/sdk/performance/" title="Performance">
-          <SidebarLink to="/sdk/performance/trace-context/">Trace Contexts</SidebarLink>
+            <SidebarLink to="/sdk/performance/trace-context/">Trace Contexts</SidebarLink>
+            <SidebarLink to="/sdk/performance/span-operations/">Span Operations</SidebarLink>
           </SidebarLink>
           <SidebarLink to="/sdk/event-payloads/" title="Event Payloads">
             <SidebarLink to="/sdk/event-payloads/transaction/">
