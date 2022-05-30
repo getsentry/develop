3ac9e469c1e8eee4246de2e31895478dfde42811
Anton Pirker <anton.pirker@sentry.io>
2022-05-30 16:31:25 +0200

    First version of serverless documentation

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -251,6 +251,9 @@ export default () => {
           </SidebarLink>
           <SidebarLink to="/sdk/sessions/">Sessions</SidebarLink>
           <SidebarLink to="/sdk/client-reports/">Client Reports</SidebarLink>
+          <SidebarLink to="/sdk/serverless/" title="Serverless SDKs">
+            <SidebarLink to="/sdk/serverless/aws-lambda">AWS Lambda</SidebarLink>   
+          </SidebarLink>
           <SidebarLink to="/sdk/research/performance">
             Research: Performance Monitoring API
           </SidebarLink>
