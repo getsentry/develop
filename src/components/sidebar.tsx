cf1adcf85e31996635b1712aedde3c0bc39d478f
Pierre Massat <pierre.massat@sentry.io>
2023-03-14 15:10:11 -0400

    Rename to Profiles

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -223,7 +223,7 @@ export default () => {
           <SidebarLink to="/sdk/research/performance">
             Research: Performance Monitoring API
           </SidebarLink>
-          <SidebarLink to="/sdk/sample-format/">Sample format for Profiling</SidebarLink>
+          <SidebarLink to="/sdk/profiles/">Profiles</SidebarLink>
         </ul>
       </li>
       <li className="mb-3" data-sidebar-branch>
