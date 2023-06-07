c9935c3bdf7a576ad68d83a92b6468aa004a4f01
Colleen O'Rourke <colleen@sentry.io>
2023-06-07 15:49:50 -0700

    ref(integrations): Add Heroku docs to sidebar (#962)

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -252,6 +252,7 @@ export default () => {
           <SidebarLink to="/integrations/bitbucket/">Bitbucket</SidebarLink>
           <SidebarLink to="/integrations/github/">GitHub</SidebarLink>
           <SidebarLink to="/integrations/gitlab/">GitLab</SidebarLink>
+          <SidebarLink to="/integrations/heroku/">Heroku</SidebarLink>
           <SidebarLink to="/integrations/jira/">Jira</SidebarLink>
           <SidebarLink to="/integrations/jira-server/">Jira Server</SidebarLink>
           <SidebarLink to="/integrations/msteams/">Microsoft Teams</SidebarLink>
