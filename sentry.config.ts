788bb764da38dc4279355b606e268e1eeae55d9f
Abhijeet Prasad <aprasad@sentry.io>
2023-07-05 13:54:27 -0400

    don't run in server

--- sentry.config.ts
+++ sentry.config.ts
@@ -2,33 +2,42 @@ import * as Sentry from '@sentry/gatsby';
 
 const activeEnv = process.env.GATSBY_ENV || process.env.NODE_ENV || 'development';
 
-Sentry.init({
-  dsn:
-    activeEnv === 'development'
-      ? undefined
-      : 'https://f107f3f0deb544289e4e056922e5e5a4@o1.ingest.sentry.io/5266138',
-  release: process.env.GATSBY_SENTRY_RELEASE,
-  integrations: [
-    new Sentry.BrowserTracing({
-      _experiments: {
-        enableInteractions: true,
-        // If you want automatic route transactions in react or similar
-        onStartRouteTransaction: Sentry.onProfilingStartRouteTransaction,
-      },
-    }),
-    new Sentry.BrowserProfilingIntegration(),
-  ],
+function isNodeEnv(): boolean {
+  return (
+    Object.prototype.toString.call(typeof process !== 'undefined' ? process : 0) ===
+    '[object process]'
+  );
+}
 
-  // @ts-expect-error this is not part of the browser SDK options yet
-  profilesSampleRate: 1.0,
+if (!isNodeEnv()) {
+  Sentry.init({
+    dsn:
+      activeEnv === 'development'
+        ? undefined
+        : 'https://f107f3f0deb544289e4e056922e5e5a4@o1.ingest.sentry.io/5266138',
+    release: process.env.GATSBY_SENTRY_RELEASE,
+    integrations: [
+      new Sentry.BrowserTracing({
+        _experiments: {
+          enableInteractions: true,
+          // If you want automatic route transactions in react or similar
+          onStartRouteTransaction: Sentry.onProfilingStartRouteTransaction,
+        },
+      }),
+      new Sentry.BrowserProfilingIntegration(),
+    ],
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
+    // @ts-expect-error this is not part of the browser SDK options yet
+    profilesSampleRate: 1.0,
 
-  // Capture Replay for 10% of all sessions,
-  // plus for 100% of sessions with an error
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-});
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+
+    // Capture Replay for 10% of all sessions,
+    // plus for 100% of sessions with an error
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+  });
+}
